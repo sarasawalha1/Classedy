92bc9605ef655fc5b854a69eb22e4e93daf85a8d
Ahmad <95480585+1poco@users.noreply.github.com>
2022-06-28 10:09:46 +0300

    Update App.js

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -11,17 +11,15 @@ function App() {
 class NavBar extends react.component{
   render(){
   return(
-  <div class="topnav">
-  <a class="active"       href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
-
-<div style="padding-left:16px">
-  <h2>Top Navigation Example</h2>
-  <p>Some content..</p>
-</div>
+  <><div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div><div style="padding-left:16px">
+        <h2>Top Navigation Example</h2>
+        <p>Some content..</p>
+      </div></>
 )
 }}
 
