df045838c29906cdc276dca09f0ae499b9165db0
BasilZreaq <95477135+BasilZreaq@users.noreply.github.com>
2022-06-28 10:19:31 +0300

    .

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,28 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+
 function App() {
   return (
     <div className="App">
-        <p>hi</p>
+      <image src = ""></image>
     </div>
   );
 }
-
-class NavBar extends react.component{
-  render(){
-  return(
-  <div class="topnav">
-  <a class="active"       href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
-
-<div style="padding-left:16px">
-  <h2>Top Navigation Example</h2>
-  <p>Some content..</p>
-</div>
-)
-}}
 
 export default App;
