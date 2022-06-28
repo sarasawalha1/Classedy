3d2d0edef5209a1c42d13d49c58e113de6820fae
sarasawalha1 <79332879+sarasawalha1@users.noreply.github.com>
2022-06-28 10:40:22 +0300

    header file

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <image src = "-/imgs/Screenshot 2022-06-28 101742.png"></image>
-      <h1> Classedy</h1>
     </div>
   );
 }
